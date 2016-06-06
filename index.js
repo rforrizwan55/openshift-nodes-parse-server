f7bfbb4b4ce0bd276c09b8cb872565a4bb4d0950
antt <antt001@gmail.com>
2016-06-06 20:47:52 +0300

    fix server url for dashboars

--- index.js
+++ index.js
@@ -24,35 +24,39 @@ var ip = process.env.NODE_IP || process.env.IP || 'localhost';
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 
+var serverURL = 'http://localhost:1337' + mountPath;
+if(process.env.OPENSHIFT_APP_DNS) {
+  serverURL = 'https://' + process.env.OPENSHIFT_APP_DNS  + mountPath;
+}
+
 var server = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || serverURL,  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 };
 //Init ParseDashboard
 var dashboard = {
-    apps: [
-      {
-        appId: server.appId,
-        serverURL: 'http://' + ip + ':' + port + mountPath,
-        masterKey: server.masterKey,
-        appName: process.env.WEBSITE_SITE_NAME || process.env.OPENSHIFT_APP_DNS || 'Parse Server Dashboard'
-      }
-    ],
-    users: [
-      {
-        user: server.appId,
-        pass: server.masterKey
-      }
-    ]
-  };
-  console.log(dashboard);
-  console.log(server);
+  apps: [
+    {
+      appId: server.appId,
+      serverURL: process.env.SERVER_URL || serverURL,
+      masterKey: server.masterKey,
+      appName: process.env.WEBSITE_SITE_NAME || process.env.OPENSHIFT_APP_DNS || 'Parse Server Dashboard'
+    }
+  ],
+  users: [
+    {
+      user: server.appId,
+      pass: server.masterKey
+    }
+  ]
+};
+  
 var api = new ParseServer(server);
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
