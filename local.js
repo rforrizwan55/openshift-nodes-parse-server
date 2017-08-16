44756fc1d8ed04915cfef9372cceee47209a838e
rabdul <rabdul@xpressdocs.com>
2017-08-16 14:27:36 -0500

    sd

--- local.js
+++ local.js
@@ -1,14 +1,6 @@
 module.exports = {
     server: {},
     dashboard: {
-        apps: [
-            {
-                "serverURL": "https://jntuh-schools9.rhcloud.com/parse",
-                "appId": "com.rizwan.jntuh",
-                "masterKey": "superman2",
-                "appName": "MyApp"
-            }
-        ]
         users: [
             {
                 user: 'myuser',
