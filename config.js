2c890db2f789f157feb59c4fdb87077fa0985669
rabdul <rabdul@xpressdocs.com>
2017-08-16 11:58:15 -0500

    sd

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ module.exports = {
     server: {
         enableAnonymousUsers: false,
         serverURL:'http://localhost:8080/parse'
+        publicServerURL:'https://jntuh-schools9.rhcloud.com/parse'
     },
-    dashboard: {}
+    dashboard: {
+        serverURL: 'http://localhost:8080/parse'
+    }
 }
