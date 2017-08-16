61729b310ba82064d5f12f4202b1fca37a8668cd
rabdul <rabdul@xpressdocs.com>
2017-08-16 14:19:51 -0500

    sd

--- config.js
+++ config.js
@@ -4,13 +4,5 @@ module.exports = {
         serverURL:'https://jntuh-schools9.rhcloud.com/parse',
         publicServerURL:'https://jntuh-schools9.rhcloud.com/parse'
     },
-    dashboard: {
-        "apps": [
-            { appId: 'com.rizwan.jntuh',
-                serverURL: 'https://jntuh-schools9.rhcloud.com/parse',
-                masterKey: 'superman2',
-                appName: 'Parse Server'
-            }
-            ]
-    }
+    dashboard: {}
 }
--- local.js
+++ local.js
@@ -1,6 +1,14 @@
 module.exports = {
     server: {},
     dashboard: {
+        apps: [
+            {
+                "serverURL": "https://jntuh-schools9.rhcloud.com/parse",
+                "appId": "com.rizwan.jntuh",
+                "masterKey": "superman2",
+                "appName": "MyApp"
+            }
+        ]
         users: [
             {
                 user: 'myuser',
